33ce7aefb921c435607297f9d017c8d2d101b5d2
Minedflayer <107548377+Minedflayer@users.noreply.github.com>
2026-08-28 18:04:26 +0200

    Fix: Added singleton to prevent socket exhaustion

--- src/api/CatalogApi/Program.cs
+++ src/api/CatalogApi/Program.cs
@@ -1,16 +1,13 @@
 using CatalogApi.Data;
 using Azure.Monitor.OpenTelemetry.Exporter;
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Azure.Messaging.ServiceBus;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
@@ -33,10 +30,13 @@ builder.Services.AddAuthorizationBuilder();
 builder.Services.AddDbContext<CatalogDbContext>(options =>
 {
     var connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
-    // Ensure the "SqlConnectionString" environment variable is set in local.settings.json and Azure App Settings
     options.UseSqlServer(connectionString);
+});
 
-    
+builder.Services.AddSingleton(s =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("ServiceBusConnection");
+    return new ServiceBusClient(connectionString); 
 });
 
 builder.Build().Run();
